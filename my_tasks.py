33fde94a2049441f211d392434832e8e2037d4aa
Tomer Hekmati <tomer.hekmati@redislabs.com>
2023-02-05 20:13:54 +0200

    more bugs

--- my_tasks.py
+++ my_tasks.py
@@ -14,7 +14,7 @@ def version_format_refactor(version):
     version = sum(x * (100 ** i) for i, x in enumerate(l))
     return version
 
-def set_min_redis_pack_version(module_version, module_name):
+def get_min_redis_pack_version(module_version, module_name):
     url_folder_map = {'json': 'RedisJSON',
                         'graph': 'RedisGraph',
                         'timeseries': 'RedisTimeSeries',
@@ -31,9 +31,9 @@ def set_min_redis_pack_version(module_version, module_name):
     if 'Light' in module_name:
         ramp_file = 'ramp-light.yml'
     pack = ''
-    if 'Search' in module_name:
+    if 'search' in module_name:
         pack = 'coord/pack/'
-    elif 'RedisearchLight' in module_name:
+    elif 'searchlight' in module_name:
         pack = 'pack/'
     version = module_version
     if version != 'master':
@@ -49,51 +49,51 @@ def set_min_redis_pack_version(module_version, module_name):
         pack_version += '.0'
     return pack_version
 
-@task(
-    help={
-        "module_name": "Redis module name",
-        "module_version": "The module version",
-        "module_url": "URL to download the module",
-    }
-)
-def set_permutations(
-    c,
-    module_name = None,
-    module_version = None,
-    module_url = None,
-):
-    # print(module_name)
-    # print(module_version)
-    min_cluster_version = set_min_redis_pack_version(module_version, module_name)
-    # print(f'Minimum cluster version the module support: {min_cluster_version}')
+# @task(
+#     help={
+#         "module_name": "Redis module name",
+#         "module_version": "The module version",
+#         "module_url": "URL to download the module",
+#     }
+# )
+# def set_permutations(
+#     c,
+#     module_name = None,
+#     module_version = None,
+#     module_url = None,
+# ):
+#     # print(module_name)
+#     # print(module_version)
+#     min_cluster_version = get_min_redis_pack_version(module_version, module_name)
+#     # print(f'Minimum cluster version the module support: {min_cluster_version}')
 
-    with open('parameters.yaml') as file:
-        documents = yaml.full_load(file)
-        os_s = documents['OS']
-        rs_versions = documents['RS_VERSIONS']
-        os_supported_by_modules = documents['OS_SUPPORTED_BY_MODULES']
+#     with open('parameters.yaml') as file:
+#         documents = yaml.full_load(file)
+#         os_s = documents['OS']
+#         rs_versions = documents['RS_VERSIONS']
+#         os_supported_by_modules = documents['OS_SUPPORTED_BY_MODULES']
 
-    # list of OSs:
-    # print(os_s)
-    # list of cluster versions:
-    cluster_versions = []
-    for rs_ver in rs_versions:
-        if version_format_refactor(rs_ver) >= version_format_refactor(min_cluster_version):
-            cluster_versions.append(rs_ver)
-    print(*cluster_versions)
-    # print(f'Final cluster versions to test: {cluster_versions}')
-    # return cluster_versions
-    # chosen_os_list = []
-    # for os in os_s:
-    #     print(os)
-    #     chosen_os_list.append(os)
-    # print('chosen_os_list after adding it automatically:')
-    # print(self.chosen_os_list)
-    # self.chosen_rs_versions = []
-    # for rs_version in rs_versions:
-    #     self.chosen_rs_versions.append(rs_version)
-    # print('chosen_rs_versions after adding it automatically:')
-    # print(self.chosen_rs_versions)
+#     # list of OSs:
+#     # print(os_s)
+#     # list of cluster versions:
+#     cluster_versions = []
+#     for rs_ver in rs_versions:
+#         if version_format_refactor(rs_ver) >= version_format_refactor(min_cluster_version):
+#             cluster_versions.append(rs_ver)
+#     print(*cluster_versions)
+#     # print(f'Final cluster versions to test: {cluster_versions}')
+#     # return cluster_versions
+#     # chosen_os_list = []
+#     # for os in os_s:
+#     #     print(os)
+#     #     chosen_os_list.append(os)
+#     # print('chosen_os_list after adding it automatically:')
+#     # print(self.chosen_os_list)
+#     # self.chosen_rs_versions = []
+#     # for rs_version in rs_versions:
+#     #     self.chosen_rs_versions.append(rs_version)
+#     # print('chosen_rs_versions after adding it automatically:')
+#     # print(self.chosen_rs_versions)
 
 @task(
     help={
@@ -141,3 +141,21 @@ def determine_module_support_os(
         parameters = yaml.full_load(file)
         supported = True if os in parameters["OS_SUPPORTED_BY_MODULES"][module_name]["os_supported"] else False
         run(f"echo {supported}", pty=True)
+
+@task(
+    help={
+        "module_name": "The module name",
+        "module_version": "The module version",
+        "cluster_version": "The cluster version",
+    }
+)
+def determine_module_version_support_cluster(
+    c,
+    module_name = None,
+    module_version = None,
+    cluster_version = None,
+):
+    min_cluster_version = get_min_redis_pack_version(module_version, module_name)
+    print(f'Minimum cluster version the module support: {min_cluster_version}')
+    supported = True if version_format_refactor(cluster_version) >= version_format_refactor(min_cluster_version) else False
+    run(f"echo {supported}", pty=True)
